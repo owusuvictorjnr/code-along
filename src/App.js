503b2352e00484ea7a830511744f6ed8d8d6fae7
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-09 15:02:34 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import { Button1, Button2, Button3 } from "./components/Button";
+import { Images } from "./components/Images";
+// import { Button2 } from "./components/Button";
+// import { Button3 } from "./components/Button";
+
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Button1/>
+      <Button2/>
+      <Button3/>
+      <Images />
+    </div>
+
+  ) 
+}
+
+export default App;
