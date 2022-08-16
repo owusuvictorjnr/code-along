606b0766c554330e4f3f613c457a64d9eabaaf8d
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-16 15:45:07 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import Header from "./components/Header";
-import { Images } from "./components/Images";
-import Button from "./components/Button";
-import Navbar from './components/Navbar'
-import Practice from "./components/Practice";
-import TaskManager from "./components/TaskManager"
+// import Header from "./components/Header";
+// import { Images } from "./components/Images";
+// import Button from "./components/Button";
+// import Navbar from './components/Navbar'
+// import Practice from "./components/Practice";
+// import TaskManager from "./components/TaskManager"
+import Form from "./components/Form"
 
 function App() {
   return(
     <div>
-      <TaskManager/>
+       <Form/>
+
+      {/* <TaskManager/> */}
       {/* <Navbar /> */}
       {/* <Header label="Add to Card"/> */}
       {/* <Button label="Read More"/>
