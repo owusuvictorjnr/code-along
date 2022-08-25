200bb15d2c92610693ddeda8fdfc839c213ed922
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-25 15:30:43 +0100

    Context API-1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Product({ product }) {
   return (
     <div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img
-          className="p-8 rounded-t-lg"
-          src={product.category.image}
-          alt="product image"
-        />
-      </a>
+      <div className="h-72 overflow-hidden">
+        <a href="#">
+          <img
+            className="p-8 rounded-t-lg"
+            src={product.image}
+            alt="product image"
+          />
+        </a>
+      </div>
+
       <div className="px-5 pb-5">
         <a href="#">
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
@@ -26,14 +29,14 @@ function Product({ product }) {
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
         </div> */}
         <div className="flex items-center mt-2.5 mb-5 dark:text-white">
-          {product.description}
+          {product.description.substring(0,150)}
         </div>
         <div className="flex justify-between items-center">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             ${product.price}
           </span>
           <Link
-            to = {`/product/${product.id}`}
+            to={`/product/${product.id}`}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add to cart
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   const { data } = useAxios(`https://api.escuelajs.co/api/v1/products/1`);
 
   return (
-    <div className="min-h-screen bg-orange-500 flex justify-center items-center">
+    <div className="min-h-screen bg-slate-500 flex justify-center items-center">
       <div className="max-w-5xl bg-white grid grid-cols-2 rounded-lg"> 
         <div>
           <img src={data?.category.image} alt=""/>
